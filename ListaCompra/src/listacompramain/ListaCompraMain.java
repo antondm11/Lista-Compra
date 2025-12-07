bbecc1593fe8267a8de2f5fde0e2523b412c4955
Antonio <antondm11@alumnos.ilerna.com>
2025-12-07 01:52:26 +0100

    Creación de la clase del proyecto

---
+++ ListaCompra/src/listacompramain/ListaCompraMain.java
@@ -0,0 +1,15 @@
+package listacompramain;
+
+import java.util.*;
+
+//Código para la aplicación gestora de lista de la compra. Por Antonio Duarte
+
+
+public class ListaCompraMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
